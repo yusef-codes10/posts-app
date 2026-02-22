f6e86f4a3e6a07442f4695fc0c1c2bfd528cdf67
Youcef <youcefcopy@gmail.com>
2026-02-22 14:18:01 +0100

    fetching the data on the pinia store instead

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,4 +1,28 @@
 // this is the pinia store
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useStore = defineStore('store', {})
+export const useStore = defineStore('store', () => {
+  // * state
+  const posts = ref([])
+
+  //   * actions
+  ;async () => {
+    try {
+      const response = await fetch('https://dummyjson.com/posts')
+      const data = await response.json()
+      posts.value = data.posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+        body: post.body,
+        views: post.views,
+        userId: post.userId,
+      }))
+      console.log(posts.value)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return { posts }
+})
